fc770a2a28406d4dce4754f6f6cd0cee1848c1e2
Marcin Grzywaczewski <killavus@gmail.com>
2024-12-11 03:07:50 +0100

    example: reanimated + lottie

--- App.tsx
+++ App.tsx
@@ -5,12 +5,21 @@
  * @format
  */
 
-import React from 'react';
+import React, {useCallback} from 'react';
 import {Button, SafeAreaView, StyleSheet, useColorScheme} from 'react-native';
 
 import LottieView from 'lottie-react-native';
 
 import successLottie from './success.json';
+import Animated, {
+  Easing,
+  useAnimatedProps,
+  useSharedValue,
+  withSequence,
+  withTiming,
+} from 'react-native-reanimated';
+
+const ReanimatedLottie = Animated.createAnimatedComponent(LottieView);
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -19,13 +28,33 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? 'black' : 'white',
   };
 
+  const animationProgress = useSharedValue(0.25);
+
+  const onSuccess = useCallback(() => {
+    animationProgress.value = 1;
+  }, [animationProgress]);
+
+  const lottieProps = useAnimatedProps(() => {
+    return {
+      progress: withSequence(
+        withTiming(animationProgress.value, {
+          duration: 2000,
+          easing: Easing.cubic,
+        }),
+        withTiming(0.25, {duration: 0}, () => {
+          animationProgress.value = 0.25;
+        }),
+      ),
+    };
+  }, []);
+
   return (
     <SafeAreaView style={[backgroundStyle, styles.root]}>
-      <LottieView
+      <Button onPress={onSuccess} title="Success!" />
+      <ReanimatedLottie
         style={styles.successAnimation}
         source={successLottie}
-        autoPlay
-        loop
+        animatedProps={lottieProps}
       />
     </SafeAreaView>
   );
