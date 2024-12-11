331a612f07d325a57899dd4d0982d707490a91eb
Marcin Grzywaczewski <killavus@gmail.com>
2024-12-11 02:53:35 +0100

    example: lottie animation

--- App.tsx
+++ App.tsx
@@ -5,32 +5,12 @@
  * @format
  */
 
-import React, {useCallback, useState} from 'react';
-import {
-  Button,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  useWindowDimensions,
-} from 'react-native';
-import Animated, {
-  Easing,
-  FadeIn,
-  FadeInUp,
-  FadeOut,
-  FadeOutDown,
-  interpolate,
-  interpolateColor,
-  LinearTransition,
-  useAnimatedRef,
-  useAnimatedStyle,
-  useDerivedValue,
-  useScrollViewOffset,
-  withSpring,
-} from 'react-native-reanimated';
-import {LoremIpsum} from './LoremIpsum';
+import React from 'react';
+import {Button, SafeAreaView, StyleSheet, useColorScheme} from 'react-native';
+
+import LottieView from 'lottie-react-native';
+
+import successLottie from './success.json';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -39,24 +19,14 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? 'black' : 'white',
   };
 
-  const [visible, setVisible] = useState(false);
-
   return (
     <SafeAreaView style={[backgroundStyle, styles.root]}>
-      <Animated.View
-        layout={LinearTransition.duration(500).easing(Easing.elastic(5))}>
-        <Button
-          onPress={useCallback(() => setVisible(value => !value), [])}
-          title="Toggle"
-        />
-        {visible ? (
-          <Animated.View
-            entering={FadeInUp.duration(500)}
-            exiting={FadeOutDown.duration(2000)}
-            style={styles.box}
-          />
-        ) : null}
-      </Animated.View>
+      <LottieView
+        style={styles.successAnimation}
+        source={successLottie}
+        autoPlay
+        loop
+      />
     </SafeAreaView>
   );
 }
@@ -64,33 +34,13 @@ function App(): React.JSX.Element {
 export default App;
 
 const styles = StyleSheet.create({
-  main: {
-    paddingVertical: 12,
-    paddingHorizontal: 20,
-  },
-  box: {
-    margin: 20,
-    width: 100,
-    height: 100,
-    borderRadius: 12,
-    backgroundColor: 'red',
-    alignSelf: 'center',
-  },
   root: {
     flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
   },
-  header: {
-    height: '20%',
-    alignSelf: 'stretch',
-    justifyContent: 'flex-end',
-    paddingVertical: 8,
-    paddingHorizontal: 10,
-    backgroundColor: 'black',
-  },
-  headerText: {
-    color: 'white',
-    fontSize: 24,
-    fontWeight: 'bold',
+  successAnimation: {
+    width: 200,
+    height: 200,
   },
 });
